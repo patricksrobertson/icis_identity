17a585fc9f3e12cfb99e8e7cd0f0f6bfb23ff40e
Patrick Robertson <patricksrobertson@gmail.com>
2013-03-29 12:06:30 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IcisIdentity::Application.config.secret_token = '26b64e0c120bb743722c6b572a8d7041082a17a48cbce3ed30a8045d450343a7835d3e171f255ed179a2ba837c73dfb8ccd8e981bf0b6d9d292e2d29526468f5'
