a3b83fa54057bf55bc03f25574a748ec6581be21
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-05 09:02:03 -0400

    Add a 'me' endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ IcisIdentity::Application.routes.draw do
   devise_for :users
   use_doorkeeper
 
+  namespace :api do
+    namespace :v1 do
+      get '/me' => "credentials#me"
+    end
+  end
+
   root :to => 'doorkeeper/applications#index'
 end
